d171f9390d2d89e73b4f5f44564bb2ea50ad61ca
Sir_Boops <admin@boops.org>
2020-10-25 15:48:45 -0600

    add regen button

--- password/js.js
+++ password/js.js
@@ -7,25 +7,34 @@ var ans = "abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ<,>./?\"
 var an = "abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 function main() {
+  genAns()
+  genAn()
+  genAnss()
+}
 
+function genAns() {
   document.getElementById("box1_0").value = generatePassword(ans, 25);
   document.getElementById("box1_1").value = generatePassword(ans, 25);
   document.getElementById("box1_2").value = generatePassword(ans, 25);
   document.getElementById("box1_3").value = generatePassword(ans, 25);
   document.getElementById("box1_4").value = generatePassword(ans, 25);
+}
 
-  document.getElementById("box2_0").value = generatePassword(an, 25);
-  document.getElementById("box2_1").value = generatePassword(an, 25);
-  document.getElementById("box2_2").value = generatePassword(an, 25);
-  document.getElementById("box2_3").value = generatePassword(an, 25);
-  document.getElementById("box2_4").value = generatePassword(an, 25);
-
+function genAnss() {
   document.getElementById("box3_0").value = generatePassword(anss, 25);
   document.getElementById("box3_1").value = generatePassword(anss, 25);
   document.getElementById("box3_2").value = generatePassword(anss, 25);
   document.getElementById("box3_3").value = generatePassword(anss, 25);
   document.getElementById("box3_4").value = generatePassword(anss, 25);
-};
+}
+
+function genAn() {
+  document.getElementById("box2_0").value = generatePassword(an, 25);
+  document.getElementById("box2_1").value = generatePassword(an, 25);
+  document.getElementById("box2_2").value = generatePassword(an, 25);
+  document.getElementById("box2_3").value = generatePassword(an, 25);
+  document.getElementById("box2_4").value = generatePassword(an, 25);
+}
 
 function onClick() {
   if(document.getElementById("length").value > 128 || document.getElementById("length").value < 1) {
